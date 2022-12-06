050adf3843b5e329d30bacb372bb880f024c9511
Mathieu Laversin <mathieulaversin@outlook.fr>
2022-12-06 09:56:11 +0100

    second commit

--- files/plot.py
+++ files/plot.py
@@ -1,13 +1,10 @@
-import numpy as np 
 import matplotlib.pyplot as plt
-import pandas as pd 
 
-def plot_learning_curves(history):
-    df = pd.DataFrame(history.history, index=np.array(history.epoch)+1)
-    df.plot(figsize=(10, 10), fontsize=12)
-    plt.grid(True)
-    plt.gca().set_ylim(0, 4)
-    plt.xlabel('epochs', fontsize=12)
-    plt.title ('learning curves', fontsize=12)
-    plt.legend(fontsize=12)
-    plt.show()
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
